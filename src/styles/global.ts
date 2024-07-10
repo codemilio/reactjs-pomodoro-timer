04a8bea0f94602e0eca9cbe75df2cece812c1f2e
Carlos Silva <carlosemilio.contato@gmail.com>
2024-07-09 22:28:21 -0300

    chore: use global style

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #333;
+    color: #FFF;
+  }
+
+  button {
+    border: 0;
+    color: #FFF;
+  }
+`
