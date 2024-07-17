32a42d2ad3d3cc2f7c30463286e9ca4305994211
Carlos Silva <carlosemilio.contato@gmail.com>
2024-07-16 23:55:33 -0300

    feat: history bage style

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,35 @@
-export function History(){
-  return(
-    <h1>History</h1>
+import { HistoryContainer, HistoryList } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>History</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Commitar</th>
+              <th>20min</th>
+              <th>Hoje</th>
+              <th>Em andamento</th>
+            </tr>
+            <tr>
+              <th>Commitar</th>
+              <th>20min</th>
+              <th>Hoje</th>
+              <th>Em andamento</th>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
