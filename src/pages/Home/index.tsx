cb26a8d69b5e9503eb320d49ea62b15cc5b767db
Carlos Silva <carlosemilio.contato@gmail.com>
2024-07-21 00:18:27 -0300

    chore: control form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import { useForm } from 'react-hook-form'
 
 import { Play } from 'phosphor-react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import zod from 'zod'
 import {
   HomeContainer,
   FormContainer,
@@ -11,13 +13,28 @@ import {
   MinutesAmoutInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmout: zod.number().min(5).max(60),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmout: 5,
+    },
+  })
+
   const task = watch('task')
   const isSubmitDisabled = !task
 
-  const handleCreateNewSycle = (data: any) => {
+  const handleCreateNewSycle = (data: NewCycleFormData) => {
     console.log(data)
+    reset()
   }
 
   return (
@@ -57,7 +74,11 @@ export function Home() {
           <span>0</span>
         </ContdownContainer>
 
-        <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
+        <StartCountdownButton
+          type="submit"
+          disabled={isSubmitDisabled}
+          onClick={() => console.log('clicou')}
+        >
           <Play size={24} />
           Começar
         </StartCountdownButton>
